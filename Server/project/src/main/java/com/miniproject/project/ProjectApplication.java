4b9167ad94949c306d1f5950480540cb2aed4e18
I339628 <muhil.kennedy@sap.com>
2019-11-29 15:32:49 +0530

    Flyway db migration enabled

--- Server/project/src/main/java/com/miniproject/project/ProjectApplication.java
+++ Server/project/src/main/java/com/miniproject/project/ProjectApplication.java
@@ -2,16 +2,18 @@ package com.miniproject.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import com.miniproject.util.LogUtil;
 
 /**
  * @author muhilkennedy
  *
- * Starting point of the microservice.
+ * Starting point of the spring-boot microservice.
  */
 
 @SpringBootApplication
+@EntityScan(basePackages = { "com.miniproject.domain" })
 @ComponentScan("com.miniproject.controller")
 public class ProjectApplication {
 	public static void main(String[] args) {
